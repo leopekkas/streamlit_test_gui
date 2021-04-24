585d56d5156b8370ed9d49278364bf953a966eac
Leopekka Saraste <leopekkas@gmail.com>
2021-04-25 00:11:29 +0300

    test

--- README.md
+++
@@ -1,3 +0,0 @@
-# Streamlit test
-
-A test for a streamlit-based (link) GUI-application with Python for plotting CSV data.
--- main/test.py
+++ main/test.py
@@ -10,3 +10,4 @@ st.sidebar.header('User input')
 uploaded_file = st.sidebar.file_uploader("Upload your input file (CSV)")
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
+
