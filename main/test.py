1e4e00db4f036b7cdf363fa2d5b9ca809f946308
Leopekka Saraste <leopekkas@gmail.com>
2021-04-25 15:58:29 +0300

    Test csv plot working

--- main/test.py
+++ main/test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib as pt
+import matplotlib.pyplot as plt
 
 st.title("Test application")
 
@@ -11,3 +11,24 @@ uploaded_file = st.sidebar.file_uploader("Upload your input file (CSV)")
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 
+    fig = plt.figure()
+
+    ax = fig.add_subplot(1,1,1)
+    ax.scatter(
+        input_df['Girth'],
+        input_df['Height']
+    )
+
+    ax.set_xlabel("Girth (in)")
+    ax.set_ylabel("Height (ft)")
+
+    #x = input_df['Girth']
+    #y = input_df['Height']
+    #plt.plot(x, y)
+
+    st.write(fig)
+
+
+#fig, axs = plt.subplots(2, sharex=True)
+#msft.plot("Index", "Girth (in)", ax=axs[0])
+#msft.plot("Index", "Height (ft)", ax=axs[1])
