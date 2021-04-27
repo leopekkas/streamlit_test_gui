0c4e4ef32034592aafb93be3baa26728514b43ce
Leopekka Saraste <leopekkas@gmail.com>
2021-04-27 22:16:23 +0300

    Add: line plot, scatter plot selectbox, the ability to choose axis values

--- main/test.py
+++ main/test.py
@@ -2,33 +2,65 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 st.title("Test application")
 
-st.sidebar.header('User input')
+st.sidebar.header('Configuration')
 
-uploaded_file = st.sidebar.file_uploader("Upload your input file (CSV)")
+plot_type = st.sidebar.radio(
+    "Plot type",
+    ('Scatter plot', 'Line plot'))
+
+uploaded_file = st.sidebar.file_uploader("Upload your input file (CSV)", type='csv')
 if uploaded_file is not None:
+
+    header_list = ["Index", "Girth", "Height", "Volume"]
     input_df = pd.read_csv(uploaded_file)
 
+    x_value = st.selectbox(
+    "X-axis",
+    ('Girth (in)', 'Height (ft)', 'Volume (cm^3)'))
+
+    y_value = st.selectbox(
+    "Y-axis",
+    ('Girth (in)', 'Height (ft)', 'Volume (cm^3)'))
+
+    x_label = ''
+    y_label = ''
+
+    if x_value == 'Girth (in)':
+        x_label = 'Girth'
+    elif x_value == 'Height (ft)':
+        x_label = 'Height'
+    else:
+        x_label = 'Volume'
+
+    if y_value == 'Girth (in)':
+        y_label = 'Girth'
+    elif y_value == 'Height (ft)':
+        y_label = 'Height'
+    else:
+        y_label = 'Volume'
+
     fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
 
-    ax = fig.add_subplot(1,1,1)
-    ax.scatter(
-        input_df['Girth'],
-        input_df['Height']
-    )
+    if plot_type == 'Scatter plot':
+        ax.scatter(
+            input_df[x_label],
+            input_df[y_label]
+        )
 
-    ax.set_xlabel("Girth (in)")
-    ax.set_ylabel("Height (ft)")
+    elif plot_type == 'Line plot':
+        ax.plot(
+            input_df[x_label],
+            input_df[y_label]
+        )
 
-    #x = input_df['Girth']
-    #y = input_df['Height']
-    #plt.plot(x, y)
+    #endif
+
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
 
     st.write(fig)
-
-
-#fig, axs = plt.subplots(2, sharex=True)
-#msft.plot("Index", "Girth (in)", ax=axs[0])
-#msft.plot("Index", "Height (ft)", ax=axs[1])
